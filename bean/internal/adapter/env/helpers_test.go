06154ff45a5425bbcf9e99fd36674e8a520c8d69
Nizar <nizarmah@hotmail.com>
2024-01-16 00:44:29 +0400

    Improve test labels

--- bean/internal/adapter/env/helpers_test.go
+++ bean/internal/adapter/env/helpers_test.go
@@ -6,15 +6,15 @@ import (
 )
 
 func TestLookup(t *testing.T) {
-	t.Run("returns var value", func(t *testing.T) {
-		os.Setenv("var", "found")
+	t.Run("returns value", func(t *testing.T) {
+		os.Setenv("key", "value")
 
-		if v, _ := lookup("var"); v != "found" {
-			t.Errorf("expected: %s, got: %s", "found", v)
+		if v, _ := lookup("var"); v != "value" {
+			t.Errorf("expected: %s, got: %s", "value", v)
 		}
 	})
 
-	t.Run("returns error if var not found", func(t *testing.T) {
+	t.Run("returns error", func(t *testing.T) {
 		os.Clearenv()
 
 		if _, err := lookup("var"); err == nil {
