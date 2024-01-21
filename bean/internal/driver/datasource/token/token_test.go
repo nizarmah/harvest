2ef7cf7e649d752e8b1b29cfcc446506d419901e
Nizar <nizarmah@hotmail.com>
2024-01-21 16:35:57 +0400

    Fix token and user datasource tests (#47)

--- bean/internal/driver/datasource/token/token_test.go
+++ bean/internal/driver/datasource/token/token_test.go
@@ -82,8 +82,7 @@ func findUnexpired(t *testing.T, ds usecase.LoginTokenDataSource) {
 			t.Fatalf("failed to create token: %s", err)
 		}
 
-		_, err = ds.FindUnexpired(token.ID)
-		if err != nil {
+		if _, err = ds.FindUnexpired(token.ID); err != nil {
 			t.Fatalf("failed to find token: %s", err)
 		}
 
@@ -98,15 +97,33 @@ func findUnexpired(t *testing.T, ds usecase.LoginTokenDataSource) {
 			t.Errorf("expected nil token, got: %v", token)
 		}
 	})
+
+	t.Run("nonexistent_token", func(t *testing.T) {
+		if _, err := ds.FindUnexpired("nonexistent"); err == nil {
+			t.Error("expected error, got nil")
+		}
+	})
 }
 
 func delete(t *testing.T, ds usecase.LoginTokenDataSource) {
-	token, err := ds.Create("action-delete", "hashed-token")
-	if err != nil {
-		t.Fatalf("failed to create token: %s", err)
-	}
+	t.Run("existing_token", func(t *testing.T) {
+		token, err := ds.Create("action-delete", "hashed-token")
+		if err != nil {
+			t.Fatalf("failed to create token: %s", err)
+		}
 
-	if err = ds.Delete(token.ID); err != nil {
-		t.Fatalf("failed to delete token: %s", err)
-	}
+		if err = ds.Delete(token.ID); err != nil {
+			t.Fatalf("failed to delete token: %s", err)
+		}
+
+		if _, err = ds.FindUnexpired(token.ID); err == nil {
+			t.Error("expected error, got nil")
+		}
+	})
+
+	t.Run("nonexistent_token", func(t *testing.T) {
+		if err := ds.Delete("nonexistent"); err == nil {
+			t.Error("expected error, got nil")
+		}
+	})
 }
