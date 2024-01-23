98fc12354b0ee41b38b40bbe34af21666dde2968
Nizar <nizarmah@hotmail.com>
2024-01-24 01:47:32 +0400

    Add passwordless usecase user creation (#57)

--- bean/internal/usecases/passwordless/passwordless.go
+++ bean/internal/usecases/passwordless/passwordless.go
@@ -65,12 +65,30 @@ func (u *UseCase) Login(id string, password string) (*entity.User, error) {
 		return nil, fmt.Errorf("failed to compare password: %w", err)
 	}
 
-	user, err := u.users.FindByEmail(token.Email)
+	user, err := u.findOrCreateUser(token.Email)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find or create user: %w", err)
+	}
+
+	u.tokens.Delete(token.ID)
+
+	return user, nil
+}
+
+func (u *UseCase) findOrCreateUser(email string) (*entity.User, error) {
+	user, err := u.users.FindByEmail(email)
 	if err != nil {
 		return nil, fmt.Errorf("failed to find user: %w", err)
 	}
 
-	u.tokens.Delete(token.ID)
+	if user != nil {
+		return user, nil
+	}
+
+	user, err = u.users.Create(email)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create user: %w", err)
+	}
 
 	return user, nil
 }
