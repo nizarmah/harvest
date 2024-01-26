64b2a6e604683fa13d645ebe1c797640844cf96f
Nizar <nizarmah@hotmail.com>
2024-01-27 00:39:27 +0400

    Fix installing dependencies once (#66)

--- bean/bin/boot.sh
+++ bean/bin/boot.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env sh
 
 function main() {
-  install_deps
-
   migrate
 
   seed
 
   run
-}
-
-function install_deps() {
-  # tern for migration and seed
-  go install github.com/jackc/tern/v2@latest
-
-  # air for live reload
-  go install github.com/cosmtrek/air@latest
 }
 
 function migrate() {
