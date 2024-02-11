1d82a72790581bc82ac4003354f16c4bbe603ff2
Nizar <nizarmah@hotmail.com>
2024-02-11 21:03:56 +0400

    bean: Revert "Fix estimator not using interval"
    This reverts commit a672a450e3e36927dd3991d7018354418e5c0566.

--- bean/internal/usecase/estimator/utils.go
+++ bean/internal/usecase/estimator/utils.go
@@ -9,50 +9,50 @@ func getSubscriptionEstimates(sub *model.Subscription) *model.Estimates {
 
 	switch sub.Period {
 	case model.SubscriptionPeriodDaily:
-		e = getEstimatesFromDaily(sub.Amount, sub.Interval)
+		e = getEstimatesFromDaily(sub.Amount)
 	case model.SubscriptionPeriodWeekly:
-		e = getEstimatesFromWeekly(sub.Amount, sub.Interval)
+		e = getEstimatesFromWeekly(sub.Amount)
 	case model.SubscriptionPeriodMonthly:
-		e = getEstimatesFromMonthly(sub.Amount, sub.Interval)
+		e = getEstimatesFromMonthly(sub.Amount)
 	case model.SubscriptionPeriodYearly:
-		e = getEstimatesFromYearly(sub.Amount, sub.Interval)
+		e = getEstimatesFromYearly(sub.Amount)
 	}
 
 	return e
 }
 
-func getEstimatesFromDaily(amount int, interval int) *model.Estimates {
+func getEstimatesFromDaily(amount int) *model.Estimates {
 	return &model.Estimates{
-		Daily:   amount / interval,
-		Weekly:  (amount * 7) / interval,
-		Monthly: (amount * 30) / interval,
-		Yearly:  (amount * 365) / interval,
+		Daily:   amount,
+		Weekly:  amount * 7,
+		Monthly: amount * 30,
+		Yearly:  amount * 365,
 	}
 }
 
-func getEstimatesFromWeekly(amount int, interval int) *model.Estimates {
+func getEstimatesFromWeekly(amount int) *model.Estimates {
 	return &model.Estimates{
-		Daily:   amount / (7 * interval),
-		Weekly:  amount / interval,
-		Monthly: (amount * 4) / interval,
-		Yearly:  (amount * 52) / interval,
+		Daily:   amount / 7,
+		Weekly:  amount,
+		Monthly: amount * 4,
+		Yearly:  amount * 52,
 	}
 }
 
-func getEstimatesFromMonthly(amount int, interval int) *model.Estimates {
+func getEstimatesFromMonthly(amount int) *model.Estimates {
 	return &model.Estimates{
-		Daily:   amount / (30 * interval),
-		Weekly:  amount / (4 * interval),
-		Monthly: amount / interval,
-		Yearly:  (amount * 12) / interval,
+		Daily:   amount / 30,
+		Weekly:  amount / 4,
+		Monthly: amount,
+		Yearly:  amount * 12,
 	}
 }
 
-func getEstimatesFromYearly(amount int, interval int) *model.Estimates {
+func getEstimatesFromYearly(amount int) *model.Estimates {
 	return &model.Estimates{
-		Daily:   amount / (365 * interval),
-		Weekly:  amount / (52 * interval),
-		Monthly: amount / (12 * interval),
-		Yearly:  amount / interval,
+		Daily:   amount / 365,
+		Weekly:  amount / 52,
+		Monthly: amount / 12,
+		Yearly:  amount,
 	}
 }
