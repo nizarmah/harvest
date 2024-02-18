e2b79b45a3877f0b3f0883d66f7ead64823ec5e6
Nizar <nizarmah@hotmail.com>
2024-02-18 08:15:30 +0400

    bean: Address suggestions

--- bean/internal/driver/datasource/session/session.go
+++ bean/internal/driver/datasource/session/session.go
@@ -6,7 +6,8 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/whatis277/harvest/bean/internal/entity/model"
+
+  "github.com/whatis277/harvest/bean/internal/entity/model"
 
 	"github.com/whatis277/harvest/bean/internal/usecase/interfaces"
 
