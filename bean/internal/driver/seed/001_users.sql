7c2f5ab9b931a675d43b0bc6fd440f0c429c59ad
Nizar <nizarmah@hotmail.com>
2024-02-24 21:04:02 +0400

    bean: Add memberships migrations (#141)

--- bean/internal/driver/seed/001_users.sql
+++ bean/internal/driver/seed/001_users.sql
@@ -10,6 +10,10 @@ VALUES
 -- subscriptions related
 ('00000000-0000-0000-0002-000000000001', 'subscriptions'),
 ('00000000-0000-0000-0002-000000000002', 'no-subscriptions'),
+-- memberships related
+('00000000-0000-0000-0003-000000000001', 'active-membership'),
+('00000000-0000-0000-0003-000000000002', 'expired-membership'),
+('00000000-0000-0000-0003-000000000003', 'no-membership'),
 
 -- filler data
 ('10000000-0000-0000-0000-000000000001', '277@hey.com')
