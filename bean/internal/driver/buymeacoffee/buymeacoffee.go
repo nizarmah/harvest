821b8b727e231033b03ac9dc92838f231a6619a0
Nizar <nizarmah@hotmail.com>
2024-03-01 06:42:11 +0400

    bean: Address suggestions

--- bean/internal/driver/buymeacoffee/buymeacoffee.go
+++ bean/internal/driver/buymeacoffee/buymeacoffee.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/whatis277/harvest/bean/internal/entity/model"
+
 	"github.com/whatis277/harvest/bean/internal/usecase/membership"
 	"github.com/whatis277/harvest/bean/internal/usecase/passwordless"
 
